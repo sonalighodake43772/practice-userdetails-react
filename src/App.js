5c5c546daee9d92f6c9404974dc9b0082b7c7908
sonalighodake43772 <sonalighodake867>
2022-12-19 13:40:46 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
+import UserList from "./component/Users/UserList";
+import UserInput from "./component/Users/UserInput";
+const App=()=>
+{
+  
+  const [userList,setUserList] = useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const addinputHandler = (uName,uAge) => {
+    setUserList((prevUserInput) => {
+      return [...prevUserInput,{name:uName,age:uAge,id:Math.random().toString()}];
+    })
+  } 
+return(
+  <div id="user-form">
+
+<UserInput onaddinput={addinputHandler}/>
+<UserList items={userList}/>
+</div>
+)
+
 }
-
 export default App;
